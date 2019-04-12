113a92457a66b7777216fe07781c5ad631250e0c
Ken Ijiwoye <alistluxury@gmail.com>
2019-04-12 21:03:41 +0000

    update slider

--- README.md
+++ README.md
@@ -1,68 +1,27 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# eComm-FullStack (Frontend)
+*Frontend repo built using React/Redux*
+---
 
-## Available Scripts
+### Purpose for this app?
+Initially to play around with using Strapi.io's CMS and auto-generated APIs on the backend. I also wanted to interact with the data so I decided to build a react frontend and I will utilize Redux for state management(eventually.lol) 
 
-In the project directory, you can run:
+### Technology Used
+MongoDB
+NodeJS
+Strapi
+React
+Redux
+Bootstrap 4
 
-### `npm start`
+### Installation
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Clone the repo:
+`git clone https://github.com/KenIjiwoye/ecomm-fullstack-frontend.git`
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+Run `yarn install` or `npm install`
 
-### `npm test`
+I also used a proxy in `package.json` to point to my backend server: `"proxy": "http://localhost:1337`
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+## TODO
+- Finish Reviews resources
+- Install & setup Redux 
--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -2,48 +2,45 @@ import React, { Component } from 'react';
 
 export default class Carousel extends Component {
   state={
-    images: []
+    images: [],
+    isLoading: true
   }
   async componentDidMount() {
       await fetch('/sliderimages')
               .then( res => res.json())
-              .then( images => this.setState({ images }))
+              .then( images => this.setState({ images, isLoading: false }))
   }
     render() {
-      console.log(this.state.images)
+      console.log('images ==>',this.state.images)
+      if(this.state.isLoading){
+        return <h1>Loading up...</h1>
+      }
       let slider = this.state.images.map( (image, i) =>(
-        <div key={i} class="carousel-item">
-          <img style={styles.img} class="d-block img-fluid" src={image.image.url} alt="Second slide"/>
+        <div key={i} class={`carousel-item ${i == 0 ? 'active': ''}`}>
+          <img style={styles.img} class="d-block img-fluid" src={image.image.url} alt=""/>
         </div>
       ))
         return (
             <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-                <ol class="carousel-indicators">
-                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner" role="listbox">
-                  <div class="carousel-item active">
-                    <img style={styles.img} class="d-block img-fluid" src="./uploads/4058edfca086421bb648cd6907bdbe87.jpg" alt="First slide"/>
-                  </div>
-                  {slider}
-                  {/* <div class="carousel-item">
-                    <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide"/>
-                  </div>
-                  <div class="carousel-item">
-                    <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide"/>
-                  </div> */}
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner" role="listbox">
+                
+                {slider}
+                
               </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
         );
     }
 }
